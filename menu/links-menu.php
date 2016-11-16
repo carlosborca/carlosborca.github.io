5ad17f5d2b4a0fedff80e9ddbd4550270ecc61d1
carlosborca <carlosborca@gmail.com>
2016-11-16 14:56:12 -0500

    Personal Web Page

---
+++ menu/links-menu.php
@@ -0,0 +1,24 @@
+<html>
+	<body>
+		<!--
+		<head>
+			<title>Links Menu</title>
+			<link href="../styles/main.css" rel="stylesheet" type="text/css"/>
+		</head>
+		-->
+		<div class="linkbar">
+			<center>
+				<ul class="links">
+					<li><a href="index.html"/>Main</a></li>
+					<li><a href="about.html"/>About</a></li>
+					<li><a href="research.html"/>Research</a></li>
+					<li><a href="people.html"/>People</a></li>
+					<li><a href="publications.html"/>Publications</a></li>
+					<li><a href="outreach.html"/>Outreach</a></li>
+					<li><a href="resources.html"/>Resources</a></li>
+					<li><a href="indice.html"/>Espa&ntildeol</a></li>
+				</ul>
+			</center>
+		</div>
+	</body>
+</html>
